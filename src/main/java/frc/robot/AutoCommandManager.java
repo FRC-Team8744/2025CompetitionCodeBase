b21ea462714efdc9230447deef6eab28e4a6d5ac
pablo32 <pablo32@pablo.com>
2025-03-29 15:47:18 -0500

    intake (test) auto commit

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -163,7 +163,7 @@ public class AutoCommandManager {
         m_chooser.addOption("1 Piece Test", m_test);
         m_chooser.addOption("Intake Test", m_intaketest);
         m_chooser.addOption("4plrs", m_4plrs);
-        m_chooser.addOption("4pgilrs", m_4pgilrs);
+        // m_chooser.addOption("4pgilrs", m_4pgilrs);
 
         SmartDashboard.putData(m_chooser);
     }
